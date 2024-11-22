8ab01d25096acf0e2b9c93209ce72262068ec797
Anca_Sab <asabadeanu@gmail.com>
2024-11-22 12:19:59 +0200

    package.json updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,35 +18,4 @@ export const App = () => {
   );
 }
 
-// import { ContactForm } from './ContactForm/ContactForm';
-// import { ContactList } from './ContactList/ContactList';
-// import { Filter } from './Filter/Filter';
-// import { Container, Title, Heading2 } from './App.styled';
-// import { ToastContainer } from 'react-toastify'; 
-// import 'react-toastify/dist/ReactToastify.css';
 
-// export const App = () => {
-//   return (
-//     <Container>
-//       {/* Title section with phonebook emoji */}
-//       <Title>
-//         Phonebook <span role="img" aria-label="phone">📞</span>
-//       </Title>
-      
-//       {/* Contact form for adding new contacts */}
-//       <ContactForm />
-      
-//       {/* Contacts header */}
-//       <Heading2>Contacts</Heading2>
-      
-//       {/* Filter for searching contacts */}
-//       <Filter />
-      
-//       {/* List of contacts */}
-//       <ContactList />
-      
-//       {/* Toast container for showing notifications */}
-//       <ToastContainer position="top-center" />
-//     </Container>
-//   );
-// };
